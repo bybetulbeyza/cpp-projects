0933f5f24394d0114a726e4bd98645950fb552ff
bybetulbeyza <demirelbetulbeyza@gmail.com>
2025-04-19 23:07:10 +0300

    binary search edited

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int binarySearch(int* arr,int n,int size){
     int left = 0;
-    int right = size-1; //inedx starts with 0, size is 8 but last index is 7
+    int right = size-1; //index starts at 0, size is 8 but last index is 7
     while(left<=right){
-        int mid = left + (right - left)/2;
-        if(arr[mid]== n){
+        int mid = left + (right - left)/2;//find the middle index
+        if(arr[mid]== n){//if middle element equal to target return middle index
             return(mid);
-        }else if(arr[mid]<n){
+        }else if(arr[mid]<n){//if middle element is smaller than target ignore left half
             left = mid+1;
         }
-        else{
+        else{//if middle element is greater than target ignore right half
             right= mid-1;
         }
     }
-    return(-1);
+    return(-1);//if the element not is not found return -1
 }
 
 int main(){
-    int arr[]= {1,3,4,5,6,7,8,9};
+    int arr[]= {1,3,4,5,6,7,8,9};//need to be sorted array for binarry search
     int n = 6;
     int size=sizeof(arr)/sizeof(arr[0]);
     int a = binarySearch(arr,n,size);
