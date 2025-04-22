ecec18adc717db8e50c020fa639bd17080e7f85c
bybetulbeyza <demirelbetulbeyza@gmail.com>
2025-04-22 23:16:05 +0300

    edit recursive fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -11,7 +11,7 @@ int main(){
     int number;
     cin>>number;
     cout<<"fibonacci series : ";
-    for (int i = 0; i<=5; i++)
+    for (int i = 0; i<=number; i++)
     cout<<fibonacci(i)<<" ";
     return 0;
 }
